6bc9a0306da66fb0424680d4e998314fa2e40a0f
jakartasipirok <kevinroni2002@gmail.com>
2022-04-15 21:24:00 +0700

    version 1.0

--- solver.py
+++ solver.py
@@ -7,61 +7,59 @@ test.close()
 unique_list = []
 possible_words = []
 
-def is_exist(letter, list):
+def is_exist(included, list):
       for words in list:
-            for letters in words:
-                  if letters == letter:
-                        possible_words.append(words)
-                        break
+            found = True
+            for letter in included:
+                  if letter not in words:
+                        found = False  
+            if found == True:
+                  possible_words.append(words)
 
-def is_not_exist(letter, list):
-      for words in list:
-            for letters in words:
-                  if letters == letter:
-                        list.remove(words)
-                        break
+def is_not_exist(letter, list1):
+      for words in list(list1):
+            if letter in words:
+                  list1.remove(words)
 
-def make_unique(list):
-      for x in list:
-          if x not in unique_list:
-              unique_list.append(x)
-
-def find_known(letter, loc, list):
-      for words in list:
+def find_known(letter, loc, list1):
+      for words in list(list1):
             if words[loc-1] != letter:
-                  list.remove(words)
-            break
+                  list1.remove(words)
 
-def del_known(letter, loc, list):
-      for words in list:
+def del_known(letter, loc, list1):
+      for words in list(list1):
             if words[loc-1] == letter:
-                  list.remove(words)
-            break
-
-exist_letter = input("include: ")
-for i in exist_letter:
-      is_exist(i, data_into_list)
-not_exist_letter = input("exclude: ")
-for i in not_exist_letter:
-      is_not_exist(i, possible_words)
+                  list1.remove(words)
 
 while True:
-      done = input("any known location(Y/N): ")
-      if done == "N":
-            break
-      elif done == "Y":
-            letter = input("letter? ")
-            location = int(input("location? "))
-            find_known(letter, location, possible_words)
+      exist_letter = input("include: ")
+      is_exist(exist_letter, data_into_list)
+      not_exist_letter = input("exclude: ")
+      for i in not_exist_letter:
+            is_not_exist(i, possible_words)
 
-while True:
-      done = input("any known false location(Y/N): ")
-      if done == "N":
-            break
-      elif done == "Y":
-            letter1 = input("letter? ")
-            location1 = int(input("location? "))
-            del_known(letter1, location1, possible_words)
+      while True:
+            done = input("any known location(y/n): ")
+            if done == "n":
+                  break
+            elif done == "y":
+                  letter = input("letter? ")
+                  location = int(input("location? "))
+                  find_known(letter, location, possible_words)
 
-print("Possible Words: ")
-print(possible_words)
+      while True:
+            done = input("any known false location(y/n): ")
+            if done == "n":
+                  break
+            elif done == "y":
+                  letter1 = input("letter? ")
+                  location1 = int(input("location? "))
+                  del_known(letter1, location1, possible_words)
+
+      print("Possible Words: ")
+      print(possible_words)
+      cont = input("continue(y/n): ")
+      if cont == "n":
+            break
+      else:
+            data_into_list = possible_words
